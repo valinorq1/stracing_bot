60256b606faa0a5881d9402c57defb7edbaeaf92
valinor <valinorProg@gmail.com>
2021-02-12 15:37:59 +0200

    editing utils.py to be closing to pep8

--- utils.py
+++ utils.py
@@ -1,40 +1,37 @@
 # -*- coding: utf-8 -*-
-import re
 import lxml.html
 from urllib.parse import urlencode
 
 
-
-
-def form_encode(form,half_link):
+def form_encode(form, half_link):
     root = lxml.html.fromstring(form)
     data = {}
     for el in root.xpath('.//input'):
         data[el.get('name')] = el.get('value')
     qstr = urlencode(data)
-    form_str = (f'{half_link}?{qstr}')
-    #semi_link = (qstr+'?') + half_link
+    form_str = f'{half_link}?{qstr}'
     new_str = form_str[:-48]
     return new_str
 
-def fix_wh(form,half_link):
-    root = lxml.html.fromstring(form)
 
+def fix_wh(form, half_link):
+    root = lxml.html.fromstring(form)
     data = {}
     for el in root.xpath('.//input'):
         data[el.get('name')] = el.get('value')
     qstr = urlencode(data)
-    form_str = (f'{half_link}?{qstr}')
+    form_str = f'{half_link}?{qstr}'
     new_str = form_str[:-54]
     return new_str
 
-def OpenBox(form,half_link):
+
+def OpenBox(form, half_link):
     root = lxml.html.fromstring(form)
     data = {}
     for el in root.xpath('.//input'):
         data[el.get('name')] = el.get('value')
     qstr = urlencode(data)
-    form_str = (f'{half_link}?{qstr}')
+    form_str = f'{half_link}?{qstr}'
     return form_str
 
 
